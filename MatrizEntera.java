1eeb81abf13bcdbc8e45b748b7e67a7a572110e0
mony527 <m.jblasco@alumnos.upm.es>
2023-11-21 14:01:41 +0100

    comentarios hechos

--- MatrizEntera.java
+++ MatrizEntera.java
@@ -8,7 +8,7 @@ import java.util.Random;
  * aleatoriamente la matriz, imprimirla por pantalla o
  * en fichero y trasponerla.
  *
- * @author Mónica
+ * @author Mónica Jiménez
  * @author Alicia Garcia
  * @version 1.0
  */
@@ -38,16 +38,34 @@ import java.util.Random;
             this.FILAS = filas;
             matriz = new int[filas][columnas];
         }
-        public int getValor(int fila, int columna) {
-            return matriz[fila][columna];
-        }
-        public void iniciarAleatorio(Random rand) {
-            for (int f=0; f<FILAS; f++)
-                for (int c=0; c<COLUMNAS; c++) {
-                    matriz[f][c] = rand.nextInt();
-                }
-        }
 
+    /**
+     * Devuelve el valor de una celda de la matriz.
+     *
+     * @param fila el número de fila de la celda de la matriz
+     * @param columna el número de la columna de la celda de la matriz
+     * @return the valor
+     */
+    public int getValor(int fila, int columna) {
+            return matriz[fila][columna];
+    }
+
+    /**
+     * Inicializa la matriz con valores aleatorios.
+     *
+     * @param rand el generador de números aleatorios
+     */
+    public void iniciarAleatorio(Random rand) {
+        for (int f = 0; f < FILAS; f++){
+            for (int c = 0; c < COLUMNAS; c++) {
+                matriz[f][c] = rand.nextInt();
+            }
+        }
+    }
+
+    /**
+     * Imprime la matriz por salida de teclado.
+     */
     public void imprimir() {
         for (int f=0; f<FILAS; f++) {
             for (int c = 0; c < COLUMNAS; c++) {
@@ -56,6 +74,13 @@ import java.util.Random;
             System.out.println();
         }
     }
+
+    /**
+     * Imprime la matriz en un fichero de texto.
+     *
+     * @param fichero nombre del fichero de texto
+     * @throws IOException erro de escritura en el fichero
+     */
     public void imprimirEnFichero(String fichero) throws IOException {
         PrintWriter pw = new PrintWriter(fichero);
         for (int f=0; f<FILAS; f++) {
