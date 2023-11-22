eb0ff1579b201e950b0e3aefa03d13e16f7658cb
mony527 <m.jblasco@alumnos.upm.es>
2023-11-22 11:45:44 +0100

    calculo

--- Calculo.java
+++ Calculo.java
@@ -4,10 +4,15 @@ public class Calculo {
             n1=10;
             n2=20;
             n3=30;
+            System.out.println("n1= "+n1+"n2= "+n2+"n3= "+n3);
             n1= n1+n2+n3;
+            System.out.println("n1= "+n1);
             n2=n3 / 10 + 4 * n1;
+            System.out.println("n2= "+n2);
             n3++;
+            System.out.println("n3= "+n3);
             n1=n2=n3+1;
+            System.out.println("n1=n2=n3= "+n1);
         }
 
 }
