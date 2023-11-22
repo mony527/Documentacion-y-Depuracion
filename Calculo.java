689adbfc6f2067d1552cd0af781a714321929d97
mony527 <m.jblasco@alumnos.upm.es>
2023-11-22 11:32:27 +0100

    matriz

---
+++ Calculo.java
@@ -0,0 +1,13 @@
+public class Calculo {
+        public static void main(String[] args) {
+            int n1, n2, n3;
+            n1=10;
+            n2=20;
+            n3=30;
+            n1= n1+n2+n3;
+            n2=n3 / 10 + 4 * n1;
+            n3++;
+            n1=n2=n3+1;
+        }
+
+}
